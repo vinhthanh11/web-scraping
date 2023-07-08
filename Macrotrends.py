1fb7b78d35748af35b1e989c7bda9e514eedd7a0
Thanh Le <letruongvinhthanh@gmail.com>
2023-07-08 14:53:51 +0700

    Update Macrotrends.py
    First, thanks for providing the scraper. I thought that was extremely useful for people in finance. To pay attributes, I've made some changes to the script by adding some functions for convenience. Here are some changes that I've made: 
    
    Instead of exporting .csv, I export to excel file. (trivial change)
    
    Multiple URLs and Sheets: This change was made in the main() function. The script was modified to scrape data from multiple URLs (Balance Sheet, Cash Flow Statement, Key Metrics, and Income Statement) and write the data to separate sheets in the same Excel file.
    
    Ticker Placeholder: A placeholder for ticker symbols was added to the main() function. The script now accepts a list of ticker symbols and iterates over them to generate the URLs for scraping and to name the Excel files.
    
    Company Name Placeholder: This change was also made in the main() function. This is to retrieve the quarterly data.
    
    Row Index Formatting: The clean_row_index(df) function was added to clean up the row index.
    
    Numeric Data Type Conversion: The convert_to_numeric(df) function was added to convert the numbers to numeric data type.
    
    Text Alignment: The align_text(filename, sheet_names) function was added to align the text to the left in the first column.
    
    Number Formatting: The format_numbers(filename, sheet_names) function was added to format the numbers to have two decimal places.
    
    Column Width Autofitting: The autofit_column_widths(filename, sheet_names) function was added to autofit the column widths.

--- Macrotrends.py
+++ Macrotrends.py
@@ -15,7 +15,10 @@ import json
 import pandas as pd
 import requests
 import os
-os.chdir('k:/')
+from openpyxl import load_workbook
+from openpyxl.styles import Alignment, numbers
+
+os.chdir('D:/UMASS AMHERST/My Courses/SOP Summer/Seeking Alpha/Macrotrends')
 
 
 #simply scrape
@@ -48,16 +51,90 @@ def etl(response):
     
     return df
 
+# Function to clean up row index
+def clean_row_index(df):
+    df.index = df.index.str.replace('-', ' ').str.title()
+    return df
 
-def main():
+# Function to convert strings to numeric data type
+def convert_to_numeric(df):
+    df = df.apply(pd.to_numeric, errors='ignore')
+    return df
+
+# Function to align text to the left in the first column of each sheet
+def align_text(filename, sheet_names):
+    wb = load_workbook(filename)
+    for sheet in sheet_names:
+        ws = wb[sheet]
+        for cell in ws['A']:
+            cell.alignment = Alignment(horizontal='left')
+    wb.save(filename)
+
+# Function to format numbers
+def format_numbers(filename, sheet_names):
+    wb = load_workbook(filename)
+    for sheet in sheet_names:
+        ws = wb[sheet]
+        for row in ws.iter_rows(min_row=2, min_col=2):
+            for cell in row:
+                cell.number_format = numbers.FORMAT_NUMBER_00
+    wb.save(filename)
+
+# Function to autofit column widths
+def autofit_column_widths(filename, sheet_names):
+    wb = load_workbook(filename)
+    for sheet in sheet_names:
+        ws = wb[sheet]
+        for column in ws.columns:
+            max_length = 0
+            column = [cell for cell in column]
+            for cell in column:
+                try:
+                    if len(str(cell.value)) > max_length:
+                        max_length = len(cell.value)
+                except:
+                    pass
+            adjusted_width = (max_length + 2)
+            ws.column_dimensions[column[0].column_letter].width = adjusted_width
+    wb.save(filename)
+
+
+def main(tickers):
     
-    url='https://www.macrotrends.net/stocks/charts/AAPL/apple/financial-statements'
-    response=scrape(url)
-    df=etl(response)
-    df.to_csv('aapl financial statements.csv')
+    for ticker, company in tickers.items():
+        urls = [
+            # Links to annual financial statements
+            f'https://www.macrotrends.net/stocks/charts/{ticker}/{company}/income-statements?freq=A',
+            f'https://www.macrotrends.net/stocks/charts/{ticker}/{company}/balance-sheet?freq=A',
+            f'https://www.macrotrends.net/stocks/charts/{ticker}/{company}/cash-flow-statement?freq=A',
+            f'https://www.macrotrends.net/stocks/charts/{ticker}/{company}/financial-ratios?freq=A',
+            # Links to quarterly financial statements
+            f'https://www.macrotrends.net/stocks/charts/{ticker}/{company}/income-statement?freq=Q',
+            f'https://www.macrotrends.net/stocks/charts/{ticker}/{company}/balance-sheet?freq=Q',
+            f'https://www.macrotrends.net/stocks/charts/{ticker}/{company}/cash-flow-statement?freq=Q',
+            f'https://www.macrotrends.net/stocks/charts/{ticker}/{company}/financial-ratios?freq=Q'
+        ]
+        sheet_names = ['Income Statement Annually', 'Balance Sheet Annually', 'Cash Flow Statement Annually', 'Key Metrics Annually',
+                       'Income Statement Quarterly', 'Balance Sheet Quarterly', 'Cash Flow Statement Quarterly', 'Key Metrics Quarterly'
+                       ]
+        filename = f'{ticker}_financial_statements.xlsx'
+        
+        with pd.ExcelWriter(filename) as writer:
+            for url, sheet_name in zip(urls, sheet_names):
+                response=scrape(url)
+                df=etl(response)
+                df = df.transpose()  # Transpose the dataframe
+                df = clean_row_index(df)  # Clean up row index
+                df = convert_to_numeric(df)  # Convert strings to numeric data type
+                df.to_excel(writer, sheet_name=sheet_name)
+        
+        align_text(filename, sheet_names)
+        format_numbers(filename, sheet_names)
+        autofit_column_widths(filename, sheet_names)
     
     return
 
 
 if __name__ == "__main__":
-    main()
+    tickers = {'AAPL': 'apple', 'PLTR': 'palantir-technologies', 'MSFT': 'microsoft', 'GOOG': 'google'}  # Add more tickers as needed
+    main(tickers)
